8a5b27e770bd06d262e161c0bbc40805300f4371
karthikselvakumar <karthikselvakumar7@gmail.com>
2012-11-22 23:50:16 +0530

    Initial commit with ROR framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuyaVuthavi::Application.config.secret_token = 'c003ba89826f5f703ab8967ddf95b8c79fefc4297692f2884531496c76011d4ca4bd00ce6b9c925c6e417073436187965defc32b159947417eb96c5b320a6f2a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SuyaVuthavi::Application.config.session_store :cookie_store, key: '_suya_vuthavi_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SuyaVuthavi::Application.config.session_store :active_record_store
