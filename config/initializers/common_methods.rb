05a615462aa0851f05acd97bbca61b6a157e3270
Govindasamy <Govindasamy.Krishnan@Connecta.se>
2013-03-06 11:19:48 +0530

    test

--- config/initializers/common_methods.rb
+++ config/initializers/common_methods.rb
@@ -1,5 +1,5 @@
-a = Account.find_by_name("Expences")
-EXPENSES_ACC_ID = a ? a.id : nil
+# a = Account.find_by_name("Expences")
+EXPENSES_ACC_ID = 1 #a ? a.id : nil
 Int_Percentage = 1
 
 def get_interest(amount, percentage=Int_Percentage)
