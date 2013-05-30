668c2d7d39520ce019db35ff2e5fc3db9f621690
Govindasamy <pkgobi@gmail.com>
2013-05-30 17:09:50 +0530

    1Rs interest added for 50 or more than 50

--- config/initializers/common_methods.rb
+++ config/initializers/common_methods.rb
@@ -1,7 +1,8 @@
-# a = Account.find_by_name("Expences")
-EXPENSES_ACC_ID = 1 #a ? a.id : nil
+exp_acc = Account.find_by_name("Expences")
+EXPENSES_ACC_ID = exp_acc ? exp_acc.id : nil
+
 Int_Percentage = 1
 
 def get_interest(amount, percentage=Int_Percentage)
-  amount * percentage / 100
+  (amount * percentage / 100) + ((amount % 100) >= 50 ? 1 : 0)
 end
